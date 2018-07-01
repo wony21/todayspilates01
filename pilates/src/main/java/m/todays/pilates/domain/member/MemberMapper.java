d73410a5015b64b48c76e10e8e8d1ed3fc18fc72
최재원 <33944910+wony21@users.noreply.github.com>
2018-07-01 22:25:16 +0900

    Revert "원격git upload"
    This reverts commit bfcff4ef8eb05d6f9da5259e0875bcc3efd198c3.

--- pilates/src/main/java/m/todays/pilates/domain/member/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package m.todays.pilates.domain.member;
-
-import java.util.List;
-import java.util.Map;
-
-public interface MemberMapper {
-	
-	List getMember();
-}
--- pilates/src/main/java/m/todays/pilates/domain/stor/StorMapper.java
+++
@@ -1,9 +0,0 @@
-package m.todays.pilates.domain.stor;
-
-import java.util.List;
-import java.util.Map;
-
-public interface StorMapper {
-	
-	List getStor();
-}
